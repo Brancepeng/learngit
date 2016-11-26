ee34c74448cbc8f176f06987fa1bf068f5495027
彭总城 <pengzongcheng@pengzongchengdeMacBook-Pro.local>
2016-11-27 03:33:12 +0800

    test commit

---
+++ learngit.js
@@ -0,0 +1,13 @@
+/**
+ * Created by pengzongcheng on 2016/11/18.
+ */
+
+function test(){
+    console.log('Hello World!');
+}
+function test1(){
+    console.log('Hello World!');
+}
+function test1(){
+    console.log('Hello World!');
+}
